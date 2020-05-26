66f898f0241e0b5c888b9ed84790307a3ebae4d0
Amir Hasan <amirhsn896@gmail.com>
2020-05-26 22:16:12 +0530

    added a table with for loop

--- my-zeroth-app/src/app/app.module.ts
+++ my-zeroth-app/src/app/app.module.ts
@@ -5,20 +5,66 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { PracticingDirectivesComponent } from './practicing-directives/practicing-directives.component'
+import { PracticingDirectivesComponent } from './practicing-directives/practicing-directives.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {RecipeListComponent} from './recipe/recipe-list/recipe-list.component';
+import {RecipeDetailComponent} from './recipe/recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipe/recipe-list/recipe-item/recipe-item.component'
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    PracticingDirectivesComponent
+    PracticingDirectivesComponent,
+    ShoppingListComponent,
+    RecipeComponent,
+    HeaderComponent,
+    ShoppingEditComponent,
+    RecipeListComponent ,
+    RecipeDetailComponent,
+    RecipeItemComponent  
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- my-zeroth-app/src/app/servers/servers.component.ts
+++ my-zeroth-app/src/app/servers/servers.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface DummyResponse{
+  biscuit:string;
+  chai:string;
+  namkeen:string;
+}
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -12,17 +18,34 @@ export class ServersComponent implements OnInit {
   serverName = '';
   isSeverNameEmpty:boolean=true;
   statusColor:String='';
+  dummyResponse:DummyResponse[]=[];
+  biscuitName:string;
   constructor() { 
     setTimeout(()=>{
       this.allowNewServer=true;
     },2000);
 
     if(this.serverName.length>0) this.isSeverNameEmpty=false;
+    for(var i=0;i<3;i++){
+    let dummyvalues={
+      biscuit:"Marie Gold"+i,
+      chai:"Bagh Bakri"+i,
+      namkeen:"aloo bhujia"+i
+    }
+    this.dummyResponse.push(dummyvalues);
+  }
+    
   }
 
   ngOnInit(): void {
   }
 
+  dummyTablepopulate(biscuit:string){
+    this.biscuitName=biscuit;
+    // this.snackbar.open(this.biscuitName,'HELLO BELLo', {
+    //   duration:1000
+    // });
+  }
   onCreateServer(param:string):void{
     console.log(param);
     this.serverCreationStatus = "server started";
